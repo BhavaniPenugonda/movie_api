06b36828f2f5efb2c31bc2868b202a223f17d266
BhavaniPenugonda <bhavani0202@gmail.com>
2024-09-12 11:42:18 +0200

    code is written using mongoose to add new user

--- index.js
+++ index.js
@@ -101,9 +101,32 @@ app.get('/directors/:Name', async (req, res) => {
 
 
 //Allow new users to register
-app.post('/users', (req, res) => {
-  res.send('Successful POST request to register a new user.');
+app.post('/users', async (req, res) => {
+  await Users.findOne({ Username: req.body.Username })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Username + 'already exists');
+      } else {
+        Users
+          .create({
+            Username: req.body.Username,
+            Password: req.body.Password,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+          })
+          .then((user) =>{res.status(201).json(user) })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).send('Error: ' + error);
+        })
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
 });
+
 
 //Allow users to update their user info (username)
 app.put('/users/:username', (req, res) => {
